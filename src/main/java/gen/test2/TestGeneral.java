67fe5af72782d011bc4157e6733609eb0613f21c
Tennison Capra <tennison_capra@hotmail.com>
2022-02-06 11:17:54 -0300

    minor changes

--- src/main/java/gen/test2/TestGeneral.java
+++ src/main/java/gen/test2/TestGeneral.java
@@ -1,6 +1,6 @@
 package gen.test2;
 
-public class Main {
+public class TestGeneral {
     public static void main(String[] args) {
         Double num = Math.pow(2.0, 3.0);
         System.out.println(num);
