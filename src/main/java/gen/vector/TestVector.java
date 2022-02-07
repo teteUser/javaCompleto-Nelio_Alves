023df6a918391312bc552ef37d43ff4c5a8f32d9
Tennison Capra <tennison_capra@hotmail.com>
2022-02-07 14:18:10 -0300

    minor changes

--- src/main/java/gen/vector/TestVector.java
+++ src/main/java/gen/vector/TestVector.java
@@ -1,5 +1,21 @@
 package gen.vector;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class TestVector {
+
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner scan = new Scanner(System.in);
+
+
+
+
+
+
+        scan.close();
+    }
+
 
 }
