8c30410a2dba4fec2a146a5f70159253ba2bb63e
Tennison Capra <tennison_capra@hotmail.com>
2022-02-07 21:59:15 -0300

    finishing Matrix exercise

--- src/main/java/general/matrix/ex100matrix/Ex100Matrix.java
+++ src/main/java/general/matrix/ex100matrix/Ex100Matrix.java
@@ -9,9 +9,28 @@ public class Ex100Matrix {
         Locale.setDefault(Locale.US);
         Scanner scan = new Scanner(System.in);
 
+        int m = scan.nextInt();
+        int n = scan.nextInt();
 
+        int[][] mat = new int[m][n];
+        for(int i=0; i< mat.length; i++){
+            for(int j=0; j<mat[i].length; j++){
+                mat[i][j] = scan.nextInt();
+            }
+        }
 
-
+        int x = scan.nextInt();
+        for(int i=0; i< mat.length; i++){
+            for(int j=0; j<mat[i].length; j++){
+                if(mat[i][j] == x){
+                    System.out.printf("\nPosition %d,%d:\n", i, j);
+                    if(j>0) System.out.printf("Left: %d\n", mat[i][j-1]);
+                    if(j<mat[i].length-1) System.out.printf("Right: %d\n", mat[i][j+1]);
+                    if(i>0) System.out.printf("Up: %d\n", mat[i-1][j]);
+                    if(i<mat.length-1) System.out.printf("Down: %d\n", mat[i+1][j]);
+                }
+            }
+        }
 
         scan.close();
     }
