aa7a0536bdfafbff14b1f72ef3ceca31a705d636
Tennison Capra <tennison_capra@hotmail.com>
2022-02-08 09:22:14 -0300

    adding some comment to the code

--- src/main/java/general/date/calendarpractice/CalendarPractice.java
+++ src/main/java/general/date/calendarpractice/CalendarPractice.java
@@ -3,6 +3,7 @@ package general.date.calendarpractice;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.time.Instant;
+import java.util.Calendar;
 import java.util.Date;
 
 public class CalendarPractice {
@@ -14,5 +15,18 @@ public class CalendarPractice {
         Date d = Date.from(Instant.parse("2018-06-25T15:42:07Z"));
 
         System.out.println(sdf.format(d));
+
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(d);
+        //cal.add(Calendar.HOUR_OF_DAY, 4); // Somando 4 horas na variável cal
+        //d = cal.getTime(); // Retornando a hora da variável cal para a variável d
+        //System.out.println(sdf.format(d)); // Imprimindo a variável d com a formatação no padrão sdf
+
+        int minutes = cal.get(Calendar.MINUTE);
+        int month = 1 + cal.get(Calendar.MONTH);
+
+        System.out.println("Minutes: " + minutes);
+        System.out.println("Month: " + month);
+
     }
 }
