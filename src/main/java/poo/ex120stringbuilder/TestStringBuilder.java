8f3a1a43d58f41f5b825b71c83fbfabc38b46c45
Tennison Capra <tennison_capra@hotmail.com>
2022-02-10 17:28:03 -0300

    building POO exercise from lesson #120

--- src/main/java/poo/ex120stringbuilder/TestStringBuilder.java
+++ src/main/java/poo/ex120stringbuilder/TestStringBuilder.java
@@ -1,5 +1,21 @@
 package poo.ex120stringbuilder;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class TestStringBuilder {
+
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner scan = new Scanner(System.in);
+
+
+
+
+
+
+
+        scan.close();
+    }
 
 }
