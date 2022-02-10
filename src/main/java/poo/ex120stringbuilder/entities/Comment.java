ac29afdba2a014789d5182e5be3d6b859749d460
Tennison Capra <tennison_capra@hotmail.com>
2022-02-10 17:26:39 -0300

    building POO exercise from lesson #120

--- src/main/java/poo/ex120stringbuilder/entities/Comment.java
+++ src/main/java/poo/ex120stringbuilder/entities/Comment.java
@@ -1,5 +1,20 @@
 package poo.ex120stringbuilder.entities;
 
 public class Comment {
+    private String text;
 
+    public Comment() {
+    }
+
+    public Comment(String text) {
+        this.text = text;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
 }
