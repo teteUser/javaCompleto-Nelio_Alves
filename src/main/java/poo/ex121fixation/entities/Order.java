cffc6943b4694ec29779e7cc1e96ca36f76b68ab
Tennison Capra <tennison_capra@hotmail.com>
2022-02-11 14:39:57 -0300

    building POO fixating exercise from lesson #121

--- src/main/java/poo/ex121fixation/entities/Order.java
+++ src/main/java/poo/ex121fixation/entities/Order.java
@@ -1,6 +1,12 @@
 package poo.ex121fixation.entities;
 
+import general.enums.OrderStatus;
+
+import java.util.Date;
+
 public class Order {
+    private Date moment;
+    private OrderStatus orderStatus;
 
 
 }
