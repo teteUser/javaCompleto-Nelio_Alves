83cb4f6712ec0b86c14e6e2f30deb5e49883bf1e
Tennison Capra <tennison_capra@hotmail.com>
2022-02-15 13:17:27 -0300

    building heritage and polymorphism lessons

--- src/main/java/poo/heritageandpolymorphism/application/Program.java
+++ src/main/java/poo/heritageandpolymorphism/application/Program.java
@@ -4,9 +4,12 @@ import poo.heritageandpolymorphism.entities.Account;
 import poo.heritageandpolymorphism.entities.BusinessAccount;
 import poo.heritageandpolymorphism.entities.SavingsAccount;
 
+import java.util.Locale;
+
 public class Program {
 
     public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
 
         Account acc = new Account(1001, "Alex", 0.0);
         BusinessAccount bacc = new BusinessAccount(1002, "Maria", 0.0, 500.0);
@@ -16,6 +19,27 @@ public class Program {
         Account acc1 = bacc;
         Account acc2 = new BusinessAccount(1003, "Bob", 0.0, 200.0);
         Account acc3 = new SavingsAccount(1004, "Anna", 0.0, 0.01);
+
+
+        // DOWNCASTING
+        BusinessAccount acc4 = (BusinessAccount) acc2;
+        acc4.loan(100.0);
+
+        if(acc3 instanceof BusinessAccount) {
+            BusinessAccount acc5 = (BusinessAccount) acc3;
+            acc5.loan(200.0);
+            System.out.println("Loan!");
+        }
+
+        if(acc3 instanceof SavingsAccount){
+            SavingsAccount acc5 = (SavingsAccount) acc3;
+            acc5.updateBalance();
+            System.out.println("Update!");
+        }
+
+        Account account1 = new Account(1001, "Alex", 1000.0);
+        account1.withdraw(200.0);
+        System.out.println("account1 balance: $" + String.format("%.2f", account1.getBalance()));
 
 
 
--- src/main/java/poo/heritageandpolymorphism/entities/SavingsAccount.java
+++ src/main/java/poo/heritageandpolymorphism/entities/SavingsAccount.java
@@ -25,4 +25,9 @@ public class SavingsAccount extends Account {
         balance += balance * interestRate;
     }
 
+    @Override
+    public void withdraw(double amount){
+        balance -= amount;
+    }
+
 }
