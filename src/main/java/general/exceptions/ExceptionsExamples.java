c745e460c488d4f4b41d91e5f7df69eedd40c1b8
Tennison Capra <tennison_capra@hotmail.com>
2022-02-17 14:02:19 -0300

    exceptions lessons

---
+++ src/main/java/general/exceptions/ExceptionsExamples.java
@@ -0,0 +1,31 @@
+package general.exceptions;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class ExceptionsExamples {
+
+    public static void main(String[] args){
+
+        File file = new File("D:\\temp\\in.txt");
+        Scanner scan = null;
+        try {
+            scan = new Scanner(file);
+            while(scan.hasNextLine()){
+                System.out.println(scan.nextLine());
+            }
+        }
+        catch(FileNotFoundException e){
+            System.out.println("Error opening file: " + e.getMessage());
+        }
+        finally {
+            if (scan != null) {
+                scan.close();
+            }
+            System.out.println("\nFinally block executed");
+        }
+
+    }
+
+}
