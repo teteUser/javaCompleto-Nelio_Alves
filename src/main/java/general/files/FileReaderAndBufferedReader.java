5c621cad9d500c27d6b4642b222cafe0cedaf182
Tennison Capra <tennison_capra@hotmail.com>
2022-03-03 14:24:11 -0300

    renaming FileReader class to FileReaderAndBufferedReader

--- src/main/java/general/files/FileReaderAndBufferedReader.java
+++ src/main/java/general/files/FileReaderAndBufferedReader.java
@@ -5,7 +5,7 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 
-public class FileReaderProgram {
+public class FileReaderAndBufferedReader {
 
     public static void main(String[] args) {
 
